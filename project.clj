966be97bb44629b67b88d74aae5e69029f69296d
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-03-27 08:58:11 -0400

    removed explicit unify dependency, latest leinjacker has correct version

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject lein-uberwar "0.2.1"
+(defproject lein-uberwar "0.2.2"
   :description "a Leiningen plugin for packaging WAR archives"
   :url "https://github.com/yogthos/lein-uberwar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/core.unify "0.5.7"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [leinjacker "0.4.3" :exclusions [org.clojure/core.unify]]])
+  :dependencies [[org.clojure/data.xml "0.0.8"]
+                 [leinjacker "0.4.3"]])
